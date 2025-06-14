55367c339cea1f9edce4fde103209e11199ce18b
Natnael Mamuye <naty.buna@gmail.com>
2025-06-14 17:40:39 +0300

    modification

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -1,10 +1,16 @@
 import pandas as pd
+import numpy as np
 
 def load_data(filepath):
     return pd.read_csv(filepath)
 
 def clean_data(df):
-    df = df[df['TotalClaims'] > 0].copy()  # Add parentheses here to call the method
+    df = df[df['TotalClaims'] > 0].copy() 
+    df['TransactionMonth'] = pd.to_datetime(df['TransactionMonth'], errors='coerce')
+    df['TransactionYear'] = df['TransactionMonth'].dt.year
+    df['RegistrationYear'] = pd.to_numeric(df['RegistrationYear'], errors='coerce')
+    df['Vehicle_age'] = df['TransactionYear'] - df['RegistrationYear'] 
+    df['Log_TotalClaims'] = np.log(df['TotalClaims'])# Add parentheses here to call the method
     return df
 
 def save_clean_data(df, output_filepath):
